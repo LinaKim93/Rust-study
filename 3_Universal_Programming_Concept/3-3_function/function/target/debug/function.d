076808560cac21d7c4e6ca26bce99c45872d296a
LinaKim93 <release93@gmail.com>
2023-01-10 22:30:39 +0900

    del: bin file

--- 3_Universal_Programming_Concept/3-3_function/function/target/debug/function.d
+++
@@ -1,1 +0,0 @@
-/home/linakim/rust-study/3_Universal_Programming_Concept/3-3_function/function/target/debug/function: /home/linakim/rust-study/3_Universal_Programming_Concept/3-3_function/function/src/main.rs
