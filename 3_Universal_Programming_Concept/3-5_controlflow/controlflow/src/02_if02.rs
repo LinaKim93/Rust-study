71c91be51ab07962023e9d45542484f9cc61da93
LinaKim93 <release93@gmail.com>
2023-01-24 01:20:11 +0900

    chapter3-5

---
+++ 3_Universal_Programming_Concept/3-5_controlflow/controlflow/src/02_if02.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // let number = 3;
+
+    // if number {
+    //     println!("number was three");
+    // }
+
+    let number = 3;
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+}
